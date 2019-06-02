d1c73567f0fe86e69830352f86fc63339e9382d0
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2019-06-02 15:19:30 +0300

    2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
 using RDKafka
 using Test
 
-# @testset "dummy" begin
-#     @test 1 == 1
-# end
+
+const BOOTSTRAP_SERVER = "localhost:19092"
+
+
+@testset "dummy" begin
+    @test 1 == 1
+end
 
 @testset "producer smoke" begin
     p = KafkaProducer(
@@ -12,11 +16,30 @@ using Test
         dr_cb = (msg -> println("err = $(msg.err)")))
     println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     produce(p, "test", 0, "message key", "message payload")
-    produce(p, "test", "message key", "message payload")    
+    produce(p, "test", "message key", "message payload")
 end
 
 
 @testset "consumer smoke" begin
+    c = KafkaConsumer("localhost:19092", "my-consumer-group")
+    parlist = [("test", 0)]
+    subscribe(c, parlist)
+    timeout_ms = 1000
+    for i=1:20
+        msg = poll(String, String, c, timeout_ms)
+        @show msg
+    end
+end
+
+
+@testset "produce-consume" begin
+    p = KafkaProducer(
+        BOOTSTRAP_SERVER,
+        Dict("request.required.acks" => "all");
+        dr_cb = (msg -> println("err = $(msg.err)"))) 
+    produce(p, "test", 0, "key1", "payload1")
+    produce(p, "test", "key2", "payload2")
+
     c = KafkaConsumer("localhost:19092", "my-consumer-group")
     parlist = [("test", 0)]
     subscribe(c, parlist)
