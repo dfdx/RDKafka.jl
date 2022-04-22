d3dbfed29979ca6766a34426e36f5114237787c3
Andrei Zhabinski <azbs@toloka.ai>
2022-04-22 21:56:32 +0300

    Fix version in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,12 @@
 using RDKafka
 using Test
 
-a = RDKafka.rd_kafka_version() # 17105151 or 0x010500ff for 1.0.5
+a = RDKafka.rd_kafka_version() # 0x010802ff for 1.8.2
 # verify the major and minor. Any change to these should be reflected
 # by a change in Project.toml
 @test (a & 0x01000000) == 0x01000000
-@test (a & 0x00050000) == 0x00050000
+@test (a & 0x00080000) == 0x00080000
+@test (a & 0x00000200) == 0x00000200
 
 
 conf =  RDKafka.kafka_conf_new()
