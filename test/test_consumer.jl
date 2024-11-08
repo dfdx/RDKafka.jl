71dc402c19d4436801a23241930b0430dbb0a996
Chris Linton-Ford <klf@graystonetrading.com>
2024-11-08 17:55:56 +0000

    Add tests for topic name

--- test/test_consumer.jl
+++ test/test_consumer.jl
@@ -1,11 +1,17 @@
 
 @testset "smoke test" begin
-    c = KafkaConsumer("localhost:9092", "my-consumer-group")
-    parlist = [("quickstart-events", 0)]
+    topic = "quickstart-events"
+    c = KafkaConsumer("localhost:9092", "my-consumer-group", Dict("auto.offset.reset" => "earliest"))
+    parlist = [(topic, 0)]
     subscribe(c, parlist)
     timeout_ms = 1000
+    msg = nothing
     for i=1:3
-        msg = poll(String, String, c, timeout_ms)
+        while msg === nothing || msg.payload === nothing
+            msg = poll(String, String, c, timeout_ms)
+        end
         @show(msg)
+        @test msg.topic.topic == topic
+        msg = nothing
     end
 end
